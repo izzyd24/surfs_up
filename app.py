ca356c1537318264138184c9be9ca47173dff9c4
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-01 11:55:53 -0700

    Updated with flask

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 # import our flask dependency
+from cProfile import run
 from flask import Flask
 # create the flask app instance
+# name denotes name of current function; its a variable 'magic method'
+# this should tell us where code is being ran or if imported into another code
 app = Flask(__name__)
+
+# create first route
+@app.route('/')
+def hello_world():
+    return hello_world
+
+set FLASK_APP = app.py
+flask run
+
